baa4cfbf158ac63ac87b435857a29cbf5c81b376
Hugo Feijó <hugo.ofeijo@gmail.com>
2022-03-12 14:50:52 -0300

    feat: adding axon server with mongoDb

--- user.query.api/src/main/java/com/springbank/user/query/api/UserQueryApplication.java
+++ user.query.api/src/main/java/com/springbank/user/query/api/UserQueryApplication.java
@@ -1,9 +1,15 @@
 package com.springbank.user.query.api;
 
+import com.springbank.user.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@Import({ AxonConfig.class })
 public class UserQueryApplication {
 
 	public static void main(String[] args) {
